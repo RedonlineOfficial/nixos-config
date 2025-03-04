eeb4deecfa0030961e53c94d62dd3abeeb9bbe18
RedonlineOfficial <dev@redonline.me>
2025-03-04 01:48:20 -0700

    feat: add several helper functions and disko input
    Added several functions from EmergentMind's github (https://github.com/EmergentMind/nix-config/blob/dev/flake.nix).
    Removed unecessary code from template.
    Added disko flake as input.

--- flake.nix
+++ flake.nix
@@ -1,79 +1,90 @@
-{
-  description = "Your new nix config";
+{ description = "Redonline's Nixos Configuration";
 
-  inputs = {
-    # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    # You can access packages and modules from different nixpkgs revs
-    # at the same time. Here's an working example:
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
-
-    # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  } @ inputs: let
+  # --- Outputs --- #
+  outputs = { self, nixpkgs, ... } @ inputs: let
     inherit (self) outputs;
-    # Supported systems for your flake packages, shell, etc.
+    inherit (nixpkgs) lib;
+
+    # --- Architecture --- #
+    # We create a list of the most popular system architectures. In this case, ARM - aarch64-linux and x86 - x86_64-linux.
     systems = [
       "aarch64-linux"
-      "i686-linux"
       "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
     ];
-    # This is a function that generates an attribute by calling a function you
-    # pass to it, with each system as an argument
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
-    # Your custom packages
-    # Accessible through 'nix build', 'nix shell', etc
-    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-    # Formatter for your nix files, available through 'nix fmt'
-    # Other options beside 'alejandra' include 'nixpkgs-fmt'
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
-    # Your custom packages and modifications, exported as overlays
-    overlays = import ./overlays {inherit inputs;};
-    # Reusable nixos modules you might want to export
-    # These are usually stuff you would upstream into nixpkgs
-    nixosModules = import ./modules/nixos;
-    # Reusable home-manager modules you might want to export
-    # These are usually stuff you would upstream into home-manager
-    homeManagerModules = import ./modules/home-manager;
+    # We take the list created above and use it as the list for the genAttrs function.  This will map each item in the list 
+    # to a given function.  We use this down below.
+    forAllSystems = lib.genAttrs systems;
 
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    nixosConfigurations = {
-      # FIXME replace with your hostname
-      your-hostname = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          # > Our main nixos configuration file <
-          ./nixos/configuration.nix
-        ];
-      };
+    # --- nixosConfiguration Functions --- #
+
+    # Takes an input (a hostname) and builds a Nix Configuration from it. 
+    mkHost = host: {
+      ${host} = lib.nixosSystem {
+          specialArgs = { 
+	    inherit inputs outputs;
+	    # This attribute maps our own ./lib to lib.custom so we can easily call our functions in other modules;
+	    # this method makes it available to home-manager as well.
+            lib = nixpkgs.lib.extend (final: prev: { 
+		custom = import ./lib { inherit (nixpkgs) lib; }; 
+	    });
+          };
+
+	  # Finally we import our host configuration. The host configuration servers as the entry point to the rest of the system configuration.
+          modules = [ ./hosts/nixos/${host} ];
+        };
     };
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      # FIXME replace with your username@hostname
-      "your-username@your-hostname" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          # > Our main home-manager configuration file <
-          ./home-manager/home.nix
-        ];
-      };
+    # This took me a while to figure out what this function did.  The top level explaination is that it maps an attribute name (hosts) to the output of mkHost. 
+    # For more details, https://ryantm.github.io/nixpkgs/functions/library/lists/#function-library-lib.lists.foldl
+    mkHostConfigs = hosts:  lib.foldl (acc: set: acc // set) { } (lib.map (host: mkHost host ) hosts);
+
+    # Run through a directory and create a list from the attribute names of each subdirectory. 
+    readHostDirs = folder: lib.attrNames (builtins.readDir ./hosts/${folder});
+
+
+  in {
+    # --- Nixos Configurations --- #
+    # We use the three functions above in creating out nixosConfigurations.  First we read through the hosts/nixos directory and pull
+    # a list of subdirectory names that we will use as an input.  We take that list and pipe it to mkHostConfigs.  This runs it through mkHost, 
+    # building a host for each hostname found, and creating an attribute set of a hostname as the attribute name and the mkHost output as the attribute value. 
+    # This makes it equivalent to nixosConfigurations { <hostname> = lib.nixosSystem {...} }. This gives us a huge benefit of not having to update
+    # this flake everytime we add a host.
+    # Credit: EmergentMind (https://github.com/EmergentMind/nix-config/blob/dev/flake.nix)
+    nixosConfigurations = mkHostConfigs (readHostDirs "nixos");
+
+    # --- Overlays --- #
+    # Overlays are custom patches or changes we want to make to a package without having to edit the source code. 
+    overlays = import ./overlays {inherit inputs;};
+
+    # --- Custom Packages --- #
+    # We take all our custom packages and map them to a system architecture that we specified in the list above
+    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+
+    # --- Formatter --- #
+    # I'm new to using formatters and this was already here in Mistero's template, so I will leave it to play with it
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+  };
+
+  # --- Inputs --- #
+  inputs = {
+    # --- Nixos Official Repositories --- #
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-stable = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # --- External System Utilities --- #
+    # Home-manager: Declarative user configuration and dotfile management
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Disko: Declarative disk partitioning and formatting utility
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
 }
