a06cfcc415cf8ed6b4167fb9d99a5a006b61b989
RedonlineOfficial <dev@redonline.me>
2025-03-06 18:09:25 -0700

    feat: add laptop module

---
+++ modules/nixos/laptop.nix
@@ -0,0 +1,18 @@
+{ config, lib, ... }: let
+  inherit (config) myNixos;
+in {
+  config = lib.mkIf myNixos.isLaptop {
+    powerManagement.enable = true;
+
+    services = {
+      libinput.enable = true;
+      fprintd.enable = true;
+      thermald.enable = true;
+      tlp = {
+	enable = true;
+	START_CHARGE_THRESH_BAT0 = 65; # 40 and below it starts to charge
+	STOP_CHARGE_THRESH_BAT0 = 90; # 80 and above it stops charging
+      };
+    };
+  };
+}
