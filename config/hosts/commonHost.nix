fd9285d103f407c5f9c888a9c721f8c57980be02
RedonlineOfficial <dev@redonline.me>
2026-04-23 10:26:59 -0700

    chore: create commonHost.nix and move flake-file
    After generating the flake template using flake-file#dendritic, moved
    outputs and dendritic to commonHost.nix and added common host
    configuration.

---
+++ config/hosts/commonHost.nix
@@ -0,0 +1,68 @@
+{ inputs, lib, ... }: {
+  # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+  #   IMPORTS
+  # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+  imports = [ inputs.flake-file.flakeModules.default ];
+
+  # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+  #   FLAKE CONFIG
+  # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+  flake-file = {
+    # ── Inputs ─────────────────────────────────────────────────────────────
+    inputs = {
+      # Nixpkgs
+      nixpkgs.url = lib.mkDefault "github:NixOS/nixpkgs/nixos-unstable";
+
+      # Dendritic Framework
+      flake-parts.url = "github:hercules-ci/flake-parts";
+      flake-file.url = "github:vic/flake-file";
+      import-tree.url = "github:vic/import-tree";
+    };
+
+    # ── Outputs ────────────────────────────────────────────────────────────
+    outputs = "inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules)";
+  };
+
+  # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+  #   COMMON HOST MODULE
+  # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+  flake.nixosModules.hostCommon = { pkgs, ... }: {
+    # ── Boot Config ────────────────────────────────────────────────────────
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    # ── Networking ─────────────────────────────────────────────────────────
+
+    networking.networkmanager.enable = true;
+
+    # ── Time & Locale ──────────────────────────────────────────────────────
+    time.timeZone = "America/Phoenix";
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    # ── System Packages ────────────────────────────────────────────────────
+    nixpkgs.config.allowUnfree = true;
+
+    environment.systemPackages = with pkgs; [
+      neovim
+    ];
+
+    # ── Services ───────────────────────────────────────────────────────────
+    services.openssh.enable = true;
+
+    # ── System Config ──────────────────────────────────────────────────────
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    nix.gc = {
+      automatic = true;
+      dates = "Saturday *-*-* 23:00:00";
+      persistent = true;
+    };
+
+    nix.optimise = {
+      automatic = true;
+      dates = "Saturday *-*-* 23:00:00";
+      persistent = true;
+    };
+  };
+
+}
