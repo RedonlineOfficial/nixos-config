4bd2044b8fe24bb2a53665901dd88929e8848d2b
RedonlineOfficial <dev@redonline.me>
2026-04-23 22:01:42 -0700

    feat: add nixos-dev configuration entrypoint

---
+++ config/hosts/nixos-dev/default.nix
@@ -0,0 +1,11 @@
+{ self, inputs, ... }: {
+  flake.nixosConfigurations.hostNixos-dev = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+
+    modules = [
+      self.nixosModules.hostCommon
+      self.nixosModules.hostNixos-devConfiguration
+      self.nixosModules.userJoshua
+    ];
+  };
+};
