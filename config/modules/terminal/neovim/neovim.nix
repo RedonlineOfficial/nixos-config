e8b8f57614747c285e38d6b1b1f494bf3b9f2b02
RedonlineOfficial <dev@redonline.me>
2026-04-24 00:56:10 -0700

    refactor: move location of neovim.nix in dir structure
    Planning on breaking neovim.nix into multiple files, having its own
    directory makes the most sense

---
+++ config/modules/terminal/neovim/neovim.nix
@@ -0,0 +1,25 @@
+{ inputs, ... }: {
+  flake-file.inputs = {
+    nvf.url = "github:notashelf/nvf";
+  };
+
+  flake.homeModules.neovim = {...}: {
+    imports = [
+      inputs.nvf.homeManagerModules.default
+
+      self.homeModules.neovimOptions
+    ];
+
+    programs.nvf = {
+      enable = true;
+
+      settings = {
+        vim = {
+          vimAlias = true;
+          theme.transparent = true;
+        
+        };
+      };
+    };
+  };
+}
