234fa32e0fb2a80cbadce8051fed952adee97386
RedonlineOfficial <dev@redonline.me>
2026-04-24 07:40:05 -0700

    feat: add prompt colors and make tweaks
    Added prompt colors for all components, moved around various components
    in vcs info

--- config/modules/terminal/zsh/zshPrompt.nix
+++ config/modules/terminal/zsh/zshPrompt.nix
@@ -21,9 +21,9 @@
         else
           # Otherwise use nerd-font symbols
           SYM_PROMPT=" "
-          SYM_GIT_BRANCH=" "
-          SYM_GIT_STAGED=" "
-          SYM_GIT_UNSTAGED=" "
+          SYM_GIT_BRANCH=""
+          SYM_GIT_STAGED=""
+          SYM_GIT_UNSTAGED="󱈸"
           SYM_GIT_UNTRACKED=""
         fi
 
@@ -31,22 +31,30 @@
         autoload -Uz vcs_info
         zstyle ':vcs_info:*' enable git
         zstyle ':vcs_info:*' check-for-changes true
-        zstyle ':vcs_info:git:*' formats " ''${SYM_GIT_BRANCH}%b %m%u%c"
-        zstyle ':vcs_info:*' stagedstr "''${SYM_GIT_STAGED}"
-        zstyle ':vcs_info:*' unstagedstr "''${SYM_GIT_UNSTAGED}"
+        zstyle ':vcs_info:git:*' formats "%F{244}[ %F{cyan}''${SYM_GIT_BRANCH}%f %F{magenta}%b %F{244}]-[%f %m%u%c %F{244}]%f"
+        zstyle ':vcs_info:*' stagedstr "%F{green}''${SYM_GIT_STAGED}%f"
+        zstyle ':vcs_info:*' unstagedstr "%F{yellow}''${SYM_GIT_UNSTAGED}%f"
         zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
 
         +vi-git-untracked() {
           if git ls-files --others --exclude-standard | grep -q .; then
-            hook_com[misc]+="''${SYM_GIT_UNTRACKED}"
+            hook_com[misc]+="%F{red}''${SYM_GIT_UNTRACKED}%f"
           fi
         }
 
         precmd() {
+          local lastStatus=$?
           vcs_info
 
-          print -P "%~ ''${vcs_info_msg_0_}"
-          PROMPT="%n@%m ''${SYM_PROMPT} "
+          local promptColor
+          if [[ $lastStatus -eq 0 ]]; then
+            promptColor="green"
+          else
+            promptColor="red"
+          fi
+
+          print -P "%F{blue}%~ ''${vcs_info_msg_0_}"
+          PROMPT="%F{cyan}%n@%m%f %F{''${promptColor}''${SYM_PROMPT}%f "
         }
       '';
   };
