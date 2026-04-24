2c0465d135093588a76605f0d616e02408dfb587
RedonlineOfficial <dev@redonline.me>
2026-04-23 21:57:35 -0700

    feat: add system level configuration

---
+++ config/hosts/nixos-dev/configuration.nix
@@ -0,0 +1,11 @@
+{ self, inputs, ... }: {
+
+  flake.nixosModules.nixos-devConfiguration = { ... }: {
+    imports = [];
+
+    networking.hostName = "nixos-dev";
+
+    system.stateVersion = "25.11";
+  };
+  
+};
