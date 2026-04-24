f7c8ae47e360f582b5d5631d3b8b299a1182fd2e
RedonlineOfficial <dev@redonline.me>
2026-04-24 06:49:38 -0700

    feat: add claude code as hm package

--- config/users/joshua/home.nix
+++ config/users/joshua/home.nix
@@ -3,13 +3,17 @@
   inputs,
   ...
 }: {
-  flake.homeModules.joshuaHome = {...}: {
+  flake.homeModules.joshuaHome = {pkgs, ...}: {
     imports = [
       self.homeModules.profilesCommon
     ];
     home.username = "joshua";
     home.homeDirectory = "/home/joshua";
     home.stateVersion = "25.11";
+
+    home.packages = [
+      pkgs.claude-code
+    ];
 
     programs.home-manager.enable = true;
   };
